d54d525e747ee05aadb6e1f5866c6879c2c78846
Tin Thanh <ctthanh2318@gmail.com>
2023-09-29 17:27:17 +0700

    Model

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,13 +1,22 @@
-class SiteControllerController {
+const Course = require('./models/Course')
+
+class SiteController {
     //[GET] /search
     search(req, res) {
         res.render('search');
     }
 
     //[GET] /
-    index(req, res) {
-        res.render('home');
+    async index(req, res) {
+        try {
+            const data = await Course.find({});
+            res.json(data);
+        } catch (error) {
+            res.status(400).json({error: err});
+        }
+        // Course.find({})
+        // res.render('home');
     }
 }
 
-module.exports = new SiteControllerController();
+module.exports = new SiteController();
--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const app = express();
 const port = 3000;
 
 const route = require('./routes');
+const db = require('./config/db');
+
+//Connect to DB
+db.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
@@ -27,7 +31,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 //Routes init
-    route(app);
+route(app);
 
 // app.get('/', (req,res) => {
 //     res.render('home');
