016774f333cbd5af30398eb8ab603f721d9c7167
Tin Thanh <ctthanh2318@gmail.com>
2023-09-30 14:14:31 +0700

    Read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,4 +1,5 @@
 const Course = require('./models/Course')
+const {multipleMongooseToObject} = require('../util/mongoose')
 
 class SiteController {
     //[GET] /search
@@ -7,15 +8,12 @@ class SiteController {
     }
 
     //[GET] /
-    async index(req, res) {
-        try {
-            const data = await Course.find({});
-            res.json(data);
-        } catch (error) {
-            res.status(400).json({error: err});
-        }
-        // Course.find({})
-        // res.render('home');
+    index(req, res, next){
+        Course.find({})
+            .then(courses => {
+                res.render('home', {courses : multipleMongooseToObject(courses)})
+            })
+            .catch(next);
     }
 }
 
