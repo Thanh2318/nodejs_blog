fbb5adb8c9a0d74224ee27b405ef4e89c0b86b7d
Tin Thanh <ctthanh2318@gmail.com>
2023-10-01 16:10:43 +0700

    Course detail page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 const newsRouter = require('./news');
 const siteRouter = require('./site');
+const courseRouter = require('./courses');
 
 function route(app) {
+
+    app.use('/courses', courseRouter);
     // app.get('/news', (req,res) => {
     //     res.render('news');
     // })
